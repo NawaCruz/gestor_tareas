b374d3f96d46eb3ee9951ed240e6fd7a470844ec
Frank <71881454@continental.edu.pe>
2024-06-04 08:33:28 -0500

    Lanzamiento 1.0.0 correcto

--- src/logica/gestor_tareas.py
+++ src/logica/gestor_tareas.py
@@ -5,3 +5,11 @@ class Tarea:
         self.completada = False
 class GestorTareas:
     pass
+
+def __init__(self):
+    self.tareas = []
+def agregar_tarea(self, titulo, descripcion):
+    if not titulo:
+        raise ValueError("El título no puede estar vacío")
+    tarea = Tarea(titulo, descripcion)
+    self.tareas.append(tarea)
